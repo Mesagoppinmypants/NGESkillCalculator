f2f11a94b8c0d1c6dd641eb0b6a2d39d44c111a8
Han Solo <pressinger.gene66@gmail.com>
2016-04-16 11:36:03 -0400

    Added combobox for user to pick profession

--- src/StartUpScreen.java
+++ src/StartUpScreen.java
@@ -1,6 +1,12 @@
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import java.awt.BorderLayout;
+import javax.swing.SwingConstants;
+import java.awt.Font;
+import javax.swing.JComboBox;
+import javax.swing.JTable;
 
 public class StartUpScreen {
 
@@ -36,6 +42,26 @@ public class StartUpScreen {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JLabel lblHello = new JLabel("Welcome to the NGE Skill Calculator!");
+		lblHello.setBounds(0, 0, 434, 19);
+		lblHello.setFont(new Font("Tahoma", Font.BOLD, 15));
+		lblHello.setHorizontalAlignment(SwingConstants.CENTER);
+		frame.getContentPane().add(lblHello);
+		
+		JLabel lblPleaseSelectA = new JLabel("Please select a profession to begin.");
+		lblPleaseSelectA.setHorizontalAlignment(SwingConstants.CENTER);
+		lblPleaseSelectA.setBounds(0, 69, 434, 14);
+		frame.getContentPane().add(lblPleaseSelectA);
+		
+		JComboBox comboBox = new JComboBox();
+		comboBox.setBounds(160, 94, 114, 20);
+		frame.getContentPane().add(comboBox);
+		AddComboBoxElements();
 	}
-
+	
+	private void AddComboBoxElements() {
+		System.out.println("Hello, this is a test.");
+	}
 }
