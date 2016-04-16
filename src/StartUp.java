6c26856082debfed2a16cc13798a8d8b8ed9b05d
Han Solo <pressinger.gene66@gmail.com>
2016-04-16 11:17:49 -0400

    Started Initial Screen

--- src/StartUp.java
+++
@@ -1,9 +0,0 @@
-
-public class StartUp {
-
-	public static void main(String[] args) {
-		System.out.println("Welcome to the SWG NGE Class Calculator Project in Java!");
-
-	}
-
-}
---
+++ src/StartUpScreen.java
@@ -0,0 +1,41 @@
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class StartUpScreen {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					StartUpScreen window = new StartUpScreen();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public StartUpScreen() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
